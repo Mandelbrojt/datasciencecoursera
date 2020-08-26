11dddc64318170f5a84c6523adc73d083bdb2c17
Mandelbrojt <luis.moreno@exatec.tec.mx>
2020-08-26 07:45:54 -0500

    Nuevo ormato de la función.

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,27 +1,15 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-
-file_numbers <- vector("character", length(id))  
-  for (i in id) {
-    if (id[i] < 10) {
-      number <- paste("00", id[i], sep = "")
-      file_numbers[i] <- number
-    } else if (id[i] >= 10 & id[i] < 100) {
-      number <- paste("0", id[i], sep = "")
-      file_numbers[i] <- number
-    }
-  }
+  ## Specify the folder name
+  mydir = "specdata"
   
+  ## Create a list for all of the CSV file names stored in the folder
+  myfiles = list.files(path = mydir, pattern = "*.csv", full.names = TRUE)
   
   library(readr)
+  ## Read all CSV files in the specified folder and store them in a list
+  ## This new list contains data frames
+  df_list <- sapply(myfiles, read_csv, simplify = FALSE)
   
-  if (length(id) > 1) {
-    for (i in id) {
-      file_name <- paste(directory, "/", id, ".csv", sep = "")
-      data <- read_csv(file_name)
-    }
-  } else {
-    file_name <- paste(directory, "/", id, ".csv", sep = "")
-    data <- read_csv(file_name)
-  }
-  
+  ## Merge all data frames in the list into one data frame
+  df <- bind_rows(df_list)
 }
